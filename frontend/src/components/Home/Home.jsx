59e0594d6f39d5b10ae48749f72b75845194a2ca
yeongseo <yeongseo5952@naver.com>
2024-10-30 12:15:39 +0900

    FEAT: Header 추가
    ## 작업 내용
    Header추가하고
    my-app에서 frontend로 파일이름 변경함

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+};
+export default Home;
